40bea48c14c8f5dfb4ad8a73e759ea1bab527bf8
Aurel1701 <gantzfan96@gmail.com>
2022-05-05 15:44:59 +0200

    edit HeaderAlt

---
+++ src/components/HeaderAlt.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="container">
+    <div class="logo">
+      <img src="../assets/spotifylogo.png" alt="" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 120px;
+  background-color: rgba(46, 58, 70, 1);
+  .logo {
+    width: 90px;
+    img {
+      width: 100%;
+      padding: 15px;
+    }
+  }
+}
+</style>
